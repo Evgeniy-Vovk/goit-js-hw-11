66d8cd2610d52edb2e7891d14143c58e08e6da7c
Evgeniy-Vovk <evgeniy.vovk@ukr.net>
2025-07-21 16:37:52 +0000

    Deploying to gh-pages from @ Evgeniy-Vovk/goit-js-hw-11@9fa3c17c03344282c2025e89f5888a9687c33791 🚀

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-import{i as n,a as d,S as f}from"./assets/vendor-D8_O3--j.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();function g({webformatURL:s,largeImageURL:r,tags:i,likes:o,views:e,comments:t,downloads:a}){return`
-    <li class="gallery-item">
-      <a href="${r}">
-        <img
-          class="gallery-image"
-          src="${s}"
-          alt="${i}"
-          loading="lazy"
-        >
-      </a>
-      <div class="gallery-info">
-        <div class="gallery-info-div">
-          <p class="gallery-info-div-text">Likes</p>
-          <p class="gallery-info-div-value">${o.toLocaleString()}</p>
+import{a as y,S as g,i as s}from"./assets/vendor-DqB7j7Ix.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const p="47396340-f7005e76dc1b3bde31bf703a9",h="https://pixabay.com/api/";function b(i){const o={key:p,q:i,image_type:"photo",orientation:"horizontal",safesearch:!0};return y.get(h,{params:o}).then(r=>r.data).catch(r=>{throw console.error("Error fetching images:",r),new Error("Failed to fetch images from Pixabay.")})}const l=document.querySelector(".gallery");function L(i){function o({webformatURL:e,largeImageURL:t,tags:a,likes:m,views:d,comments:u,downloads:f}){return`<li class="gallery-item">
+        <div class="gallery-item-container">
+          <a class="gallery-link" href="${t}">
+            <img class="gallery-image" src="${e}" alt="${a}"
+          /></a>
+          <div class="image-info">
+            <div class="image-info-container">
+              <b>Likes</b>
+              <p>${m}</p>
+              </div>
+            <div class="image-info-container">
+              <b>Views</b>
+              <p>${d}</p>
+                </div>
+            <div class="image-info-container">
+              <b>Comments</b>
+              <p>${u}</p>
+                </div>
+            <div class="image-info-container">
+              <b>Downloads</b>
+              <p>${f}</p>
+                </div>
+          </div>
         </div>
-        <div class="gallery-info-div">
-          <p class="gallery-info-div-text">Views</p>
-          <p class="gallery-info-div-value">${e.toLocaleString()}</p>
-        </div>
-        <div class="gallery-info-div">
-          <p class="gallery-info-div-text">Comments</p>
-          <p class="gallery-info-div-value">${t.toLocaleString()}</p>
-        </div>
-        <div class="gallery-info-div">
-          <p class="gallery-info-div-text">Downloads</p>
-          <p class="gallery-info-div-value">${a.toLocaleString()}</p>
-        </div>
-      </div>
-    </li>
-  `}const p=document.querySelector(".form"),c=document.querySelector(".gallery"),l=document.querySelector(".loader");p.addEventListener("submit",u);function u(s){s.preventDefault();const r=s.target.elements.formInput.value.trim();if(l.classList.remove("visually-hidden"),c.innerHTML="",!r){n.show({message:"Please enter a search term.",backgroundColor:"#ef4040",position:"topRight"}),l.classList.add("visually-hidden");return}d.get("https://pixabay.com/api/",{params:{key:"47396340-f7005e76dc1b3bde31bf703a9",q:r.trim(),image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:40}}).then(i=>{const o=i.data.hits;if(!o.length)throw new Error("Sorry, there are no images matching <br /> your search query. Please try again!");const e=o.map(a=>g(a)).join("");c.innerHTML=e,new f(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250}).refresh()}).catch(i=>{n.show({message:i.message,backgroundColor:"#ef4040",position:"topRight"})}).finally(()=>{l.classList.add("visually-hidden")}),s.target.reset()}
+        </li>`}const r=i.map(o).join("");l.insertAdjacentHTML("beforeend",r),new g(".gallery-link",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"}).refresh()}function v(){l.innerHTML=""}function S(){document.querySelector(".loader").classList.remove("visuallyhidden")}function x(){document.querySelector(".loader").classList.add("visuallyhidden")}console.log("mygallery",l);const c=document.querySelector(".form"),w=document.querySelector('input[name="search-text"]');c.addEventListener("submit",P);async function P(i){i.preventDefault();const o=w.value.trim();if(o===""){c.reset(),s.error({title:"Error",message:"Search field cannot be empty!",position:"topRight"});return}v(),S();try{const r=await b(o);r.hits.length===0?s.info({message:"❌ Sorry, there are no images matching your search query. Please try again!",position:"topRight",icon:!1,close:!1,backgroundColor:"#ef4040",maxWidth:"432px",minHeight:"88px",html:!0}):L(r.hits)}catch(r){s.error({title:"Error",message:r.message||"An error occurred while fetching images. Please try again later.",position:"topRight"})}finally{x(),c.reset()}}
 //# sourceMappingURL=index.js.map
